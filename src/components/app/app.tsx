0d7a2220a421ef2cf5099bc9f6c964646ce9f123
Alexey Kozlov <aleksey.o.kozlov@rt.ru>
2022-10-25 20:23:11 +0300

    room page

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,16 +1,16 @@
-import { LoginPage } from '../../pages';
+import { MainPage } from '../../pages';
 
-// type AppSettings = {
-//   offersCount: number;
-// }
+type AppSettings = {
+  offersCount: number;
+}
 
-// const appSettings: AppSettings = {
-//   offersCount: 100
-// };
+const appSettings: AppSettings = {
+  offersCount: 100
+};
 
 function App(): JSX.Element {
-  // return <MainPage offersCount={appSettings.offersCount} />;
-  return <LoginPage />;
+  return <MainPage offersCount={appSettings.offersCount} />;
+
 }
 
 export default App;
